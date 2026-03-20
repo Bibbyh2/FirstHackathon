0ad284f3a154489cf8092f96eba4295ea68b6335
Henry Bibby <12Bibbyh2@gmail.com>
2026-03-20 14:08:46 -0400

    I GOT IT TO ACTUALLY MULTIPLY 2 TOGETHER LETS GOOOOO

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 class Matrix {
     public:
-        std::vector<std::vector<int>> Weights;
+        std::vector<std::vector<float>> Weights;
         int Rows;
         int Collumns;
         void RandomNew(int RowsC, int CollumnsC) {
@@ -11,7 +11,7 @@ class Matrix {
 
 
             for (int i = 0; i< Collumns; i++) {
-                std::vector<int> Collumn;
+                std::vector<float> Collumn;
                 for (int j = 0; j< Rows; j++) {
                     Collumn.push_back(std::rand());
 
@@ -20,7 +20,7 @@ class Matrix {
           
             }
         }
-        void SetWeights(std::vector<std::vector<int>> input) {
+        void SetWeights(std::vector<std::vector<float>> input) {
             Weights = input;
             Collumns = Weights.size();
             Rows = Weights[0].size();
@@ -33,15 +33,51 @@ class Matrix {
                 std::cout << std::endl;
           
             }
+            std::cout << std::endl;
         }
+        void Transform(Matrix T){
+            
+            if (T.Collumns == Rows){
 
+                std::vector<std::vector<float>> Product(T.Collumns,std::vector<float>(Rows,0));
+                std::cout<< "bruh?";
+                int Operations = Rows;
+                std::cout << Rows<< std::endl;
+                std::cout << T.Collumns << std::endl;
+                for (int rowT = 0; rowT < Rows; rowT++) {
+                    for (int ColW = 0; ColW < T.Collumns; ColW++) {
+                        float result = 0;
+                        for (int Comp = 0; Comp < Collumns; Comp++) {
 
+                            float num1 =  T.Weights[rowT][Comp];
+                            float num2 = Weights[Comp][ColW];
+                            result += num1 * num2;
+                            std::cout << num1 << " ";
+                            std::cout << num2 << std::endl;
+                            //std::cout<< result << std::endl;
+                        }
+                        
+                        Product[rowT][ColW] = result;
+                        std:: cout << rowT << "," << ColW << std::endl;
+                        std::cout<< Product[rowT][ColW] << std::endl;
+                    }
+                }
+              
+                Weights = Product;
+                Weights = Product;
+                Collumns = T.Collumns;
 
+            } 
+            else {
+                std::cout << "Cant be multiplied";
+
+            }
+
+        }
 };
 
 int main(){
     Matrix Test;
-    Test.RandomNew(4,5);
     Test.SetWeights(
         {
             {1,2,3,4},
@@ -49,6 +85,19 @@ int main(){
             {9,10,11,12},
         }
     );
+    Matrix Transform;
+    Transform.SetWeights(
+        {
+        {5,6,7},
+        {3,4,2},
+        {1,0,3},
+        {6,7,8}
+        }
+
+    );
+    Test.PrintMatrix();
+    Transform.PrintMatrix();
+    Test.Transform(Transform);
     Test.PrintMatrix();
 
 
